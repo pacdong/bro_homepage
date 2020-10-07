b462116bcc965c882a864f923c6930e118dca4e0
pacdong <8000pac@naver.com>
2020-10-07 16:23:23 +0900

    Before Lint Setting

--- src/App.js
+++ src/App.js
@@ -2,30 +2,34 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 import Routes from "./Routes";
 
 const GlobalStyled = createGlobalStyle`
 ${reset};
-* {
+  * {
   transition: 0.5s;
-}
-body {
-  background-color: white;
-  font-size: 16px;
-}
-a {
-        color: black;
-        text-decoration:none;
-    }
+  }
+  body {
+      background-color: white;
+      font-size: 16px;
+  }
+  a {
+      color: black;
+      text-decoration:none;
+  }
 `;
 
 function App() {
   return (
     <>
       <GlobalStyled />
+      <Header />
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
+      <Footer />
     </>
   );
 }
